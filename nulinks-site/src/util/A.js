31309f667e8b99d81ba8dc128c0135851bb3018f
Kevin Johnson <kevin@coolkev.com>
2018-08-16 16:42:09 -0400

    Update A.js

--- nulinks-site/src/util/A.js
+++ nulinks-site/src/util/A.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const isPWA = () => window.matchMedia('(display-mode: standalone)').matches;
 
 export const A = props => <a {...props} target={isPWA() ? "_blank" : "_self"} />

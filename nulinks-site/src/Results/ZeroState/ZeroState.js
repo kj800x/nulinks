d471845a81794c5f8905b708c3c724977a4aaa16
Kevin Johnson <kevin@coolkev.com>
2018-08-16 16:40:29 -0400

    Update ZeroState.js

--- nulinks-site/src/Results/ZeroState/ZeroState.js
+++ nulinks-site/src/Results/ZeroState/ZeroState.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { A } from '../../util/A';
+
 import "./ZeroState.css";
 
 import searching from './searching.svg';
@@ -9,7 +11,7 @@ export default () => (
     <h2 className="tagline">Can't find what you're looking for?</h2>
     <img className="searchingIllustration" src={searching} alt=""/> {/* SVG provided by the lovely folks over at unDraw */}
     <div className="ctas">
-      <a className="button" href="https://github.com/kj800x/nulinks/issues">Make a Suggestion</a>
+      <A className="button" href="https://github.com/kj800x/nulinks/issues">Make a Suggestion</A>
     </div>
   </li>
 )

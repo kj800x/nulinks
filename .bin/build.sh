04d8d57101d7f9a8633d2edb350dab26293d1af6
Kevin Johnson <kevin@coolkev.com>
2018-09-04 21:31:42 -0400

    Add Note for CI Server to Build Scripts (#34)
    * Update build.sh
    
    * Update install.sh
    
    * Update manifest.json

--- .bin/build.sh
+++ .bin/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script must be copied to the CI server.
+# If you change it, please ask in your PR for the CI server to be updated.
+
 # Build the site
 cd nulinks-site
 npm run build
--- .bin/install.sh
+++ .bin/install.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# This script must be copied to the CI server.
+# If you change it, please ask in your PR for the CI server to be updated.
+
 cd nulinks-site
 npm install
 cd ..

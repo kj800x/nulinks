ad3c8443e21213f6f3b67a82c6a0a69cfb650869
Kevin Johnson <kevin@coolkev.com>
2019-02-25 01:44:21 -0500

    Upgrade searching algorithm to be slightly easier to understand

--- nulinks-common/src/search.js
+++ nulinks-common/src/search.js
@@ -1,37 +1,79 @@
 import { toLower } from "./util.js";
+const trim = string => string.trim();
+const stripDashes = string => string.replace(/\-/g, "");
 
-export const trim = string => string.trim();
+const enhance2 = enhancer => func => a => b => func(enhancer(a))(enhancer(b));
+
+const lowered = enhance2(toLower);
+const trimmed = enhance2(trim);
+const dashStripped = enhance2(stripDashes);
+
+const normalized = func => lowered(trimmed(dashStripped(func)));
+
 export const add = (a, b) => a + b;
 export const byScoreDesc = (a, b) => b.score - a.score;
-export const contains = string => text => trim(toLower(text)).indexOf(trim(toLower(string))) !== -1;
-export const exact = string => text => trim(toLower(string)) === trim(toLower(text));
-export const startsWith = string => text => trim(toLower(text)).startsWith(trim(toLower(string)));
 
-const scoreKeyword = text => keyword => [
-  (contains(text)(keyword) ? 1 : 0),
-  (startsWith(text)(keyword) ? 5 : 0),
-  (startsWith(text)(keyword) ? ((20 - text.length) / 20.0) : 0), // Prefer shorter
-  (exact(text)(keyword) ? 9999 : 0),
-].reduce(add, 0);
+// With space ensures that the query appears at the beginning of a word
+export const contains = normalized(query => value => (value).indexOf(query) !== -1);
+export const exact = normalized(query => value => query === value);
+export const startsWith = normalized(query => value => value.startsWith(query));
 
-const scoreKeywords = text => keywords => keywords.map((keyword) => ({
-  keyword: keyword,
-  score: scoreKeyword(text)(keyword)
-}));
+// About how often would the typical student access this site during their stay at NEU:
+const USAGE_FREQUENCY_SCALE_FACTOR = {
+  "DAILY": 3,
+  "A FEW TIMES A WEEK": 2.6,
+  "A FEW TIMES A SEMESTER": 2.3,
+  "ONCE A SEMESTER": 2,
+  "ONCE A YEAR": 1.5,
+  "ONCE": 1
+}
 
-const score = text => match => {
-  const keywordsScored = scoreKeywords(text)(match.keywords).sort(byScoreDesc);
-  return {
-    score: keywordsScored.map(function(a) {return a.score}).reduce(add, 0)
-           + scoreKeyword(text)(match.title)
-           + (scoreKeyword(text)(match.target) / 1000)
-           + (match.tuning ? (match.tuning / 100.0) : 0),
-    representativeKeyword: keywordsScored[0].keyword,
-    value: match,
+// Evaluate two strings for different properties
+const evaluateString = query => value => ({
+  exact: exact(query)(value),
+  startsWith: startsWith(query)(value),
+  contains: contains(query)(value),
+});
+
+// Score the similarity of two strings
+const scoreString = query => value => {
+  const values = evaluateString(query)(value);
+  return (values.exact * 5) + (values.startsWith * 4) + values.contains;
+}
+
+const scoreKeyword = query => keyword => ({
+  score: scoreString(query)(keyword) * 3,
+  value: keyword
+});
+
+const scoreOthers = query => link =>
+  (
+    scoreString(query)(link.title || "") +
+    scoreString(query)(link.target || "") +
+    scoreString(query)(link.description || "")
+  )
+
+const score = query => link => {
+  const keywordsScored = link.keywords.map(scoreKeyword(query)).sort(byScoreDesc);
+  const bestKeyword = keywordsScored[0];
+  const defaultKeyword = link.keywords[0];
+
+  if (bestKeyword.score > 0) { // If any keywords matched, base the score on the best keyword.
+    return {
+      score: bestKeyword.score * USAGE_FREQUENCY_SCALE_FACTOR[link.usageFrequency],
+      representativeKeyword: bestKeyword.value,
+      value: link
+    }
+  } else { // If no keywords matched, base the score on the title, description, and target.
+    return {
+      score: scoreOthers(query)(link) * USAGE_FREQUENCY_SCALE_FACTOR[link.usageFrequency],
+      representativeKeyword: defaultKeyword,
+      value: link
+    }
   }
 }
 
-export const search = data => text => data
-  .map(score(text))
-  .filter(result => result.score >= 1)
+export const search = links => query => links
+  .map(score(query))
+  .filter(result => result.score > 0)
   .sort(byScoreDesc);

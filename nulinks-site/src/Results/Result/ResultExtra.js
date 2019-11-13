7be25d91937bc9b54b92b851160449a404f2ce6c
Kevin Johnson <kevin@coolkev.com>
2019-11-12 19:55:38 -0500

    Prettier write and npm audit

--- nulinks-site/src/Results/Result/ResultExtra.js
+++ nulinks-site/src/Results/Result/ResultExtra.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import FoodTrucksExtra from './FoodTruckExtra/FoodTruckExtra';
+import FoodTrucksExtra from "./FoodTruckExtra/FoodTruckExtra";
 
 export default class ResultExtra extends Component {
   render() {
-    if (this.props.result.value.title === 'Boston Food Trucks') {
+    if (this.props.result.value.title === "Boston Food Trucks") {
       return <FoodTrucksExtra />;
     }
     return null;
